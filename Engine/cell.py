158511c5bb01e323907d1df31a69ea9a37bc01d0
James Chase <gochases@gmail.com>
2024-04-24 16:12:55 -0600

    Add cell.py file

---
+++ Engine/cell.py
@@ -0,0 +1,15 @@
+################################################################################
+# Name: James A. Chase
+# File: cell.py
+# Date: 23 April 2024
+# Description:
+#
+# Class file for Cell class.
+#
+################################################################################
+
+class Cell:
+    pass
+
+if __name__ == '__main__':
+    assert False, 'This is a class file. Import its contents into another file.'
