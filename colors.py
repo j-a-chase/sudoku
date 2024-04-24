b304d854c5960a5e99c2ce49e0692e9bf941c026
James Chase <gochases@gmail.com>
2024-04-23 18:48:38 -0600

    Add colors.py file

---
+++ colors.py
@@ -0,0 +1,14 @@
+################################################################################
+# Name: James A. Chase
+# File: colors.py
+# Date: 23 April 2024
+# Description:
+#
+# Collection of constants and useful functions for generating RGB colors.
+#
+################################################################################
+
+WHITE = (255, 255, 255)
+
+if __name__ == '__main__':
+    assert False, 'This is a module. Import its contents into another file.'
