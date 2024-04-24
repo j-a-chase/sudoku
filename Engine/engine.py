8ad345d0a8bf37484e8bff4e2cefeb5fd23b3512
James Chase <gochases@gmail.com>
2024-04-23 19:34:23 -0600

    Add lines for basic game grid placement and a debug loop to Engine

--- Engine/engine.py
+++ Engine/engine.py
@@ -14,8 +14,11 @@ from pygame import display
 from .colors import *
 
 # engine class global constants
-WINDOW_WIDTH = 800
-WINDOW_HEIGHT = 800
+WINDOW_WIDTH = 1000
+WINDOW_HEIGHT = 1000
+OFFSET = (WINDOW_WIDTH - 800) // 2
+THICK_LINE = 5
+THIN_LINE = 1
 
 class Engine:
 
@@ -44,7 +47,45 @@ class Engine:
         # initialize window
         self.window = display.set_mode([WINDOW_WIDTH, WINDOW_HEIGHT])
         self.window.fill(WHITE)
+
+        # GAME BOX LINES
+
+        # top line
+        pygame.draw.line(self.window, BLACK,
+                         (OFFSET, OFFSET),
+                         (WINDOW_WIDTH - OFFSET, OFFSET), THICK_LINE)
+        
+        # bottom line
+        pygame.draw.line(self.window, BLACK,
+                         (OFFSET, WINDOW_HEIGHT - OFFSET),
+                         (WINDOW_WIDTH - OFFSET, WINDOW_HEIGHT - OFFSET), THICK_LINE)
+        
+        # left line
+        pygame.draw.line(self.window, BLACK,
+                         (OFFSET, OFFSET),
+                         (OFFSET, WINDOW_HEIGHT - OFFSET), THICK_LINE)
+        
+        # right line
+        pygame.draw.line(self.window, BLACK,
+                         (WINDOW_WIDTH - OFFSET, OFFSET),
+                         (WINDOW_WIDTH - OFFSET, WINDOW_HEIGHT - OFFSET), THICK_LINE)
+
+        # set window caption
+        display.set_caption("Sudoku")
+
+        # apply window changes
         display.flip()
+
+        # debug loop
+        run = True
+        while run:
+            for event in pygame.event.get():
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_ESCAPE:
+                        run = False
+                elif event.type == pygame.QUIT:
+                    run = False
+        pygame.quit()
 
 if __name__ == '__main__':
     assert False, 'This is a class file. Import its contents into another file.'
