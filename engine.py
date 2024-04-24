6ca64175dbb7f86f1a624cd647351973b6dae909
James Chase <gochases@gmail.com>
2024-04-23 18:11:25 -0600

    Add engine.py file

---
+++ engine.py
@@ -0,0 +1,18 @@
+################################################################################
+# Name: James A. Chase
+# File: engine.py
+# Date: 23 April 2024
+# Description:
+#
+# Class file for game engine.
+#
+################################################################################
+
+# imports
+import pygame
+
+class Engine:
+    pass
+
+if __name__ == '__main__':
+    assert False, 'This is a class file. Import its contents into another file.'
