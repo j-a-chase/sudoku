756d542484a1d9fc7ba72760eeabcd86c9670b62
James Chase <gochases@gmail.com>
2024-04-23 18:52:21 -0600

    Add basic constructor and setup function to Engine class

--- engine.py
+++ engine.py
@@ -10,9 +10,39 @@
 
 # imports
 import pygame
+from colors import *
+
+# engine class global constants
+WINDOW_WIDTH = 800
+WINDOW_HEIGHT = 800
 
 class Engine:
-    pass
+
+    def __init__(self) -> None:
+        '''
+        Constructor
+
+        Parameters: None
+
+        Returns: None
+        '''
+        self.window = None
+        self.__setup()
+        
+    def __setup(self) -> None:
+        '''
+        Runs basic setup commands for the pygame application on startup.
+
+        Parameters: None
+
+        Returns: None
+        '''
+        # initialize pygame resources
+        pygame.init()
+
+        # initialize window
+        self.window = pygame.display.set_mode([WINDOW_WIDTH, WINDOW_HEIGHT])
+        self.window.fill(WHITE)
 
 if __name__ == '__main__':
     assert False, 'This is a class file. Import its contents into another file.'
