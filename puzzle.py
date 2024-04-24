fa2bf7d7d0950d8b19653c367e0b6f50b77b6d52
James Chase <gochases@gmail.com>
2024-04-23 18:13:25 -0600

    Add puzzle.py

---
+++ puzzle.py
@@ -0,0 +1,15 @@
+################################################################################
+# Name: James A. Chase
+# File: puzzle.py
+# Date: 23 April 2024
+# Description:
+#
+# Class file for Puzzle class.
+#
+################################################################################
+
+class Puzzle:
+    pass
+
+if __name__ == '__main__':
+    assert False, 'This is a class file. Import its contents into another file.'
