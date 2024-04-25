f4c78d9d8db209212a739d3641a2b51c4d430de4
James Chase <gochases@gmail.com>
2024-04-25 13:33:25 -0600

    Add button.py

---
+++ Engine/button.py
@@ -0,0 +1,15 @@
+################################################################################
+# Name: James A. Chase
+# File: button.py
+# Date: 23 April 2024
+# Description:
+#
+# Class file for Button class to create buttons in a pygame application.
+#
+################################################################################
+
+class Button:
+    pass
+
+if __name__ == '__main__':
+    assert False, 'This is a class file. Import its contents into another file.'
