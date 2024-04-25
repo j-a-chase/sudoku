e52013dcc8e0b29a9f0e62f5c84d905025f5a033
James Chase <gochases@gmail.com>
2024-04-25 14:29:29 -0600

    Implement functional Button class for pygame applications

--- Engine/button.py
+++ Engine/button.py
@@ -8,8 +8,70 @@
 #
 ################################################################################
 
+# imports
+import pygame
+from typing import Tuple
+
 class Button:
-    pass
+    def __init__(self, x: int, y: int, text: str, font: pygame.font.Font,
+                 color: Tuple[int, int, int], scale: float=1.0) -> None:
+        '''
+        Constructor
+
+        Parameters:
+            - x: an integer indicating the x-position where the button should be
+                    rendered
+            - y: an integer indicating the y-position where the button should be
+                    rendered
+            - text: a string indicating the text to be rendered on the button
+            - font: a pygame.Font object that indicates what font to render the
+                    button text in
+            - color: a tuple containing integer RGB values for a color
+            - scale: a float indicating a scale for the button, default 1.0
+
+        Returns: None
+        '''
+        # render provided text as an image
+        image = font.render(text, True, color)
+
+        # scale the image appropriately
+        self.img = pygame.transform.scale(image, (int(image.get_width() * scale),
+                                                    int(image.get_height() * scale)))
+        
+        # render image in a rectangle in a given position
+        self.rect = self.img.get_rect(topleft=(x, y))
+
+        # hold clicked state for the button
+        self.clicked = False
+
+    def draw(self, screen: pygame.Surface) -> bool:
+        '''
+        Draws the button on the screen
+
+        Parameters:
+            - screen: a pygame.Surface object to draw the button on
+
+        Returns:
+            - a boolean value indicating the button was pressed
+        '''
+        # indicates if the button was pressed
+        action = False
+
+        # get mouse position
+        pos = pygame.mouse.get_pos()
+
+        # check mouseover and clicks
+        if self.rect.collidepoint(pos):
+            if pygame.mouse.get_pressed()[0] == 1 and not self.clicked:
+                self.clicked = True
+                action = True
+
+        if pygame.mouse.get_pressed()[0] == 0:
+            self.clicked = False
+
+        screen.blit(self.img, (self.rect.x, self.rect.y))
+
+        return action
 
 if __name__ == '__main__':
     assert False, 'This is a class file. Import its contents into another file.'
