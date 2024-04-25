d70d5312d5e58ff8834db554510b95e010358144
James Chase <gochases@gmail.com>
2024-04-24 19:45:59 -0600

    Add light and dark gray colors

--- Engine/colors.py
+++ Engine/colors.py
@@ -13,6 +13,8 @@ BLACK = (0, 0, 0)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
+LIGHT_GRAY = (200, 200, 200)
+DARK_GRAY = (100, 100, 100)
 
 if __name__ == '__main__':
     assert False, 'This is a module. Import its contents into another file.'
