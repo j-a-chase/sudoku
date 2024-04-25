d71a57ddd88225f74dad361505ab11ab5685332b
James Chase <gochases@gmail.com>
2024-04-25 16:39:51 -0600

    Add background and border functionality to Button class, along with additional scaling attributes

--- Engine/button.py
+++ Engine/button.py
@@ -14,7 +14,8 @@ from typing import Tuple
 
 class Button:
     def __init__(self, x: int, y: int, text: str, font: pygame.font.Font,
-                 color: Tuple[int, int, int], scale: float=1.0) -> None:
+                 color: Tuple[int, int, int], scale: float=1.0,
+                 padding: int=0) -> None:
         '''
         Constructor
 
@@ -28,6 +29,8 @@ class Button:
                     button text in
             - color: a tuple containing integer RGB values for a color
             - scale: a float indicating a scale for the button, default 1.0
+            - padding: an integer indicating the number of pixels to pad the
+                       button in each direction
 
         Returns: None
         '''
@@ -35,8 +38,9 @@ class Button:
         image = font.render(text, True, color)
 
         # scale the image appropriately
-        self.img = pygame.transform.scale(image, (int(image.get_width() * scale),
-                                                    int(image.get_height() * scale)))
+        self.img = pygame.transform.scale(image,
+                                          (int(image.get_width() * scale),
+                                           int(image.get_height() * scale)))
         
         # render image in a rectangle in a given position
         self.rect = self.img.get_rect(topleft=(x, y))
@@ -44,12 +48,21 @@ class Button:
         # hold clicked state for the button
         self.clicked = False
 
-    def draw(self, screen: pygame.Surface) -> bool:
+        # holds the designated amount of padding to add to the button background
+        # and border
+        self.padding = padding
+
+    def draw(self, screen: pygame.Surface, bg_color: Tuple[int, int, int],
+             border_color: Tuple[int, int, int]) -> bool:
         '''
         Draws the button on the screen
 
         Parameters:
             - screen: a pygame.Surface object to draw the button on
+            - bg_color: a tuple containing integer RGB values for a color for the
+                        background of the button
+            - border_color: a tuple containing integer RGB values for a color
+                            for the border of the button
 
         Returns:
             - a boolean value indicating the button was pressed
@@ -69,6 +82,16 @@ class Button:
         if pygame.mouse.get_pressed()[0] == 0:
             self.clicked = False
 
+        # inflate a new rectangle for the background and border of the button
+        bg_rect = self.rect.inflate(self.padding, self.padding)
+
+        # button background
+        pygame.draw.rect(screen, bg_color, bg_rect)
+
+        # button border
+        pygame.draw.rect(screen, border_color, bg_rect, width=5)
+
+        # button text
         screen.blit(self.img, (self.rect.x, self.rect.y))
 
         return action
