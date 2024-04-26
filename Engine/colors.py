da2888db73c79adb506a8b0d174e54dfd22f7243
James Chase <gochases@gmail.com>
2024-04-25 19:13:59 -0600

    Add aqua and darker gray colors

--- Engine/colors.py
+++ Engine/colors.py
@@ -13,9 +13,11 @@ BLACK = (0, 0, 0)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
+AQUA = (0, 255, 255)
 FAINT_GRAY = (230, 230, 230)
 LIGHT_GRAY = (200, 200, 200)
 DARK_GRAY = (100, 100, 100)
+DARKER_GRAY = (50, 50, 50)
 
 if __name__ == '__main__':
     assert False, 'This is a module. Import its contents into another file.'
