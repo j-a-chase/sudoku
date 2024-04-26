01bcb3d0193f77263669d876ddadb1248634d677
James Chase <gochases@gmail.com>
2024-04-25 19:56:37 -0600

    Add lib.py file

---
+++ Engine/lib.py
@@ -0,0 +1,15 @@
+################################################################################
+# Name: James A. Chase
+# File: lib.py
+# Date: 23 April 2024
+# Description:
+#
+# A collection of helpful functions for the project that can be separately
+# tested.
+#
+################################################################################
+
+# imports
+
+if __name__ == '__main__':
+    assert False, 'This is a module. Import its contents into another file.'
