6892dbc28df55c26d6f704a4950dd302da0f80a2
James Chase <gochases@gmail.com>
2024-04-26 21:20:12 -0600

    Add conversion function for mouse position to grid position

--- Engine/lib.py
+++ Engine/lib.py
@@ -98,5 +98,83 @@ def solve(board: List[List[Cell]]) -> bool:
             stack.pop()
     return False
 
+def mouse_pos_to_grid(pos: Tuple[int, int], grid_size: int,
+                      grid_offset: int, cell_size: int) -> Tuple[int, int]:
+    '''
+    Converts the integer value of the mouse position to grid indices.
+
+    Parameters:
+        - pos: a tuple of integers containing the x and y coordinates of the
+               mouse position
+        - grid_size: an integer representing the size of the sudoku grid
+        - grid_offset: an integer representing the offset of the sudoku grid
+                       position from 0, 0 in the game window
+        - cell_size: an integer representing the size of the individual cells
+
+    Returns:
+        - a tuple of two integers representing the row and column indices the
+          cursor should be placed in.
+    '''
+    assert type(pos) == tuple, 'Parameter "pos" must be a tuple!'
+    assert type(grid_size) == int
+    assert type(grid_offset) == int
+
+    x, y = pos
+
+    assert type(x) == int, 'Parameter "pos" must contain two integers!'
+    assert type(y) == int, 'Parameter "pos" must contain two integers!'
+    
+    grid_pos_x = None
+    grid_pos_y = None
+
+    if (
+        x < grid_offset
+        or y < grid_offset
+        or x > grid_size + grid_offset
+        or y > grid_size + grid_offset
+    ): return None
+
+    if grid_offset <= x < grid_offset + cell_size:
+        grid_pos_x = 0
+    elif grid_offset + cell_size <= x < grid_offset + cell_size * 2:
+        grid_pos_x = 1
+    elif grid_offset + cell_size * 2 <= x < grid_offset + cell_size * 3:
+        grid_pos_x = 2
+    elif grid_offset + cell_size * 3 <= x < grid_offset + cell_size * 4:
+        grid_pos_x = 3
+    elif grid_offset + cell_size * 4 <= x < grid_offset + cell_size * 5:
+        grid_pos_x = 4
+    elif grid_offset + cell_size * 5 <= x < grid_offset + cell_size * 6:
+        grid_pos_x = 5
+    elif grid_offset + cell_size * 6 <= x < grid_offset + cell_size * 7:
+        grid_pos_x = 6
+    elif grid_offset + cell_size * 7 <= x < grid_offset + cell_size * 8:
+        grid_pos_x = 7
+    elif grid_offset + cell_size * 8 <= x < grid_offset + cell_size * 9:
+        grid_pos_x = 8
+
+    if grid_offset <= y < grid_offset + cell_size:
+        grid_pos_y = 0
+    elif grid_offset + cell_size <= y < grid_offset + cell_size * 2:
+        grid_pos_y = 1
+    elif grid_offset + cell_size * 2 <= y < grid_offset + cell_size * 3:
+        grid_pos_y = 2
+    elif grid_offset + cell_size * 3 <= y < grid_offset + cell_size * 4:
+        grid_pos_y = 3
+    elif grid_offset + cell_size * 4 <= y < grid_offset + cell_size * 5:
+        grid_pos_y = 4
+    elif grid_offset + cell_size * 5 <= y < grid_offset + cell_size * 6:
+        grid_pos_y = 5
+    elif grid_offset + cell_size * 6 <= y < grid_offset + cell_size * 7:
+        grid_pos_y = 6
+    elif grid_offset + cell_size * 7 <= y < grid_offset + cell_size * 8:
+        grid_pos_y = 7
+    elif grid_offset + cell_size * 8 <= y < grid_offset + cell_size * 9:
+        grid_pos_y = 8
+
+    if grid_pos_x is None or grid_pos_y is None:
+        return None
+    return grid_pos_x, grid_pos_y
+
 if __name__ == '__main__':
     assert False, 'This is a module. Import its contents into another file.'
